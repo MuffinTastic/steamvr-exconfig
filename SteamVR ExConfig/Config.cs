a9e27d4e89d088656b7b3f74dabd5fbea5dc9026
MuffinTastic <muffintastic32@gmail.com>
2023-12-26 13:51:02 -0800

    generalized config for the program

---
+++ SteamVR ExConfig/Config.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SteamVR_ExConfig;
+
+public class Config
+{
+    [JsonPropertyName( "openvrpath" ), JsonRequired]
+    public string? OpenVRRegistryFilePath { get; set; }
+
+    // --- //
+
+    private const string ConfigPath = "./config.json";
+
+    public void SaveToFile()
+    {
+        Debug.WriteLine( $"Saving config to {ConfigPath}" );
+
+        using ( var stream = File.Open( ConfigPath, FileMode.Create ) )
+        {
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+            stream.Write( Encoding.Default.GetBytes( JsonSerializer.Serialize( this, options ) ) );
+        }
+    }
+
+    // --- //
+
+    public static Config Load()
+    {
+        Config? config = null;
+
+        try
+        {
+            config = LoadFromFile();
+
+            if ( config is null )
+                Debug.WriteLine( $"Config file {ConfigPath} contained a null object" );
+        }
+        catch ( FileNotFoundException )
+        {
+            Debug.WriteLine( $"Config file {ConfigPath} not found" );
+        }
+
+        return config ?? new Config();
+    }
+
+    private static Config? LoadFromFile()
+    {
+        using ( var stream = File.OpenRead( ConfigPath ) )
+        {
+            return JsonSerializer.Deserialize<Config>( stream, JsonSerializerOptions.Default );
+        }
+    }
+}
