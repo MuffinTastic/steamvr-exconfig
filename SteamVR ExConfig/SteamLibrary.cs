915b095de6f145dc90b434c09997e8f129937ea2
MuffinTastic <muffintastic32@gmail.com>
2023-12-26 14:57:48 -0800

    reduce scope - SteamConfig → SteamLibraries

--- SteamVR ExConfig/SteamLibrary.cs
+++ SteamVR ExConfig/SteamLibrary.cs
@@ -11,39 +11,15 @@ namespace SteamVR_ExConfig;
 
 public class SteamLibrary
 {
-    public string? Path { get; set; }
-    public HashSet<string>? Apps { get; set; }
+    public required string Path { get; set; }
+    public required HashSet<string> Apps { get; set; }
 }
 
-public class SteamConfig
+public class SteamLibraries
 {
-    public string? SteamPath { get; set; }
-
-    [JsonIgnore]
-    public string? ConfigPath { get; set; }
-    [JsonIgnore]
-    public string? VRAppConfigPath { get; set; }
-
-    [JsonIgnore]
-    public List<SteamLibrary>? Libraries { get; set; }
+    public required List<SteamLibrary> Libraries { get; set; }
 
     // --- //
-
-    public void SaveToFile()
-    {
-        try
-        {
-            using ( var stream = File.Open( SteamPathFile, FileMode.Create ) )
-            {
-                var options = new JsonSerializerOptions() { WriteIndented = true };
-                stream.Write( Encoding.Default.GetBytes( JsonSerializer.Serialize( this, options ) ) );
-            }
-        }
-        catch ( Exception ex )
-        {
-            Debug.WriteLine( $"Couldn't save ExConfig file {SteamPathFile}: {ex}" );
-        }
-    }
 
     public string? GetNameForAppID ( string appID )
     {
@@ -124,93 +100,13 @@ public class SteamConfig
 
     // --- //
 
-    private const string SteamPathFile = "./steampath.json";
-
-    private const string SteamRegistrySubKey = "SOFTWARE\\WOW6432Node\\Valve\\Steam";
-    private const string DefaultWindowsSteamPath = "C:\\Program Files (x86)\\Steam";
-    private const string DefaultLinuxSteamPath = "~/.steam/steam";
-
-    private const string SteamConfigPath = "config";
-    private const string SteamVRAppConfigPath = "vrappconfig";
     private const string SteamAppsPath = "steamapps";
     private const string CommonPath = "common";
 
     private const string SteamLibraryFolderVDF = "libraryfolders.vdf";
     private const string SteamAppManifestFormat = "appmanifest_{0}.acf";
 
-    public static SteamConfig Load()
-    {
-        SteamConfig? config = null;
-
-        try
-        {
-            config = LoadFromFile();
-
-            Debug.WriteLine( $"Loaded Steam Path" );
-        }
-        catch ( Exception ex )
-        {
-            Debug.WriteLine( $"Couldn't read Steam Path {SteamPathFile} - {ex.Message}" );
-        }
-
-        if ( config is null )
-        {
-            Debug.WriteLine( "Generating Steam Path" );
-            // Get Steam path first.
-
-            var steamPath = AutoDetectSteamInstall();
-
-            if ( steamPath is null )
-            {
-                if ( RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) )
-                    steamPath = DefaultWindowsSteamPath;
-                else if ( RuntimeInformation.IsOSPlatform( OSPlatform.Linux ) )
-                    steamPath = DefaultLinuxSteamPath;
-
-                Debug.WriteLine( $"Couldn't autodetect Steam installation, falling back on default path" );
-            }
-
-            Debug.WriteLine( $"Steam path: {steamPath}" );
-
-            config = new SteamConfig()
-            {
-                SteamPath = steamPath!,
-            };
-
-            config.SaveToFile();
-        }
-
-        config.ConfigPath = Path.Combine( config.SteamPath!, SteamConfigPath );
-        config.VRAppConfigPath = Path.Combine( config.SteamPath!, SteamConfigPath, SteamVRAppConfigPath );
-
-        config.Libraries = GetSteamLibraries( config.SteamPath! );
-
-        return config;
-    }
-
-    private static SteamConfig? LoadFromFile()
-    {
-        using ( var stream = File.OpenRead( SteamPathFile ) )
-        {
-            return JsonSerializer.Deserialize<SteamConfig>( stream, JsonSerializerOptions.Default );
-        }
-    }
-
-    private static string? AutoDetectSteamInstall()
-    {
-        if ( !RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) )
-            return null;
-
-        Debug.WriteLine( "Autodetecting Steam installation from Windows Registry" );
-
-        RegistryKey? key = Registry.LocalMachine.OpenSubKey( SteamRegistrySubKey );
-        if ( key is null )
-            return null;
-
-        return key.GetValue( "InstallPath" ) as string;
-    }
-
-    private static List<SteamLibrary> GetSteamLibraries( string steamPath )
+    public static List<SteamLibrary> GetLibraries( OpenVRPaths openVRPaths )
     {
         List<SteamLibrary> libraries = new();
 
@@ -218,7 +114,7 @@ public class SteamConfig
         {
             KVObject? librariesData;
 
-            var vdfPath = Path.Combine( steamPath, SteamConfigPath, SteamLibraryFolderVDF );
+            var vdfPath = Path.Combine( openVRPaths.ConfigPath, SteamLibraryFolderVDF );
             using ( var stream = File.OpenRead( vdfPath ) )
             {
                 var kv = KVSerializer.Create( KVSerializationFormat.KeyValues1Text );
